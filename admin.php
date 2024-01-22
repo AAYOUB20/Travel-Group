e142f9b0da5859f7ea91355b7d0af41ab73c4dfb
AAYOUB20 <aliayoub2019@gmail.com>
2024-01-22 11:34:47 +0100

    ali 11

--- admin.php
+++ admin.php
@@ -1,22 +1,3 @@
-<?php
-session_start();
-
-include "SQL_connection.php"; // include il connection al database
-
-$adminQuery = "SELECT * FROM messages ORDER BY timestamp DESC";
-$adminResult = mysqli_query($conn, $adminQuery);
-
-if (!$adminResult) {
-    die("Error executing the admin query: " . mysqli_error($conn));
-}
-
-$userQuery = "SELECT firstname, lastname, email  FROM user ORDER BY email"; 
-$userResult = mysqli_query($conn, $userQuery);
-
-if (!$userResult) {
-    die("Error executing the user query: " . mysqli_error($conn));
-}
-?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -131,5 +112,23 @@ if (!$userResult) {
 </html>
 
 <?php
+session_start();
+
+include "SQL_connection.php"; // include il connection al database
+
+$adminQuery = "SELECT * FROM messages ORDER BY timestamp DESC";
+$adminResult = mysqli_query($conn, $adminQuery);
+
+if (!$adminResult) {
+    die("Error executing the admin query: " . mysqli_error($conn));
+}
+
+$userQuery = "SELECT firstname, lastname, email  FROM user ORDER BY email"; 
+$userResult = mysqli_query($conn, $userQuery);
+
+if (!$userResult) {
+    die("Error executing the user query: " . mysqli_error($conn));
+}
+
 mysqli_close($conn);
 ?>
