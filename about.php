964372445155f8ddcfb40ee006892026c3c96864
AAYOUB20 <aliayoub2019@gmail.com>
2024-01-22 11:18:31 +0100

    ali10

--- about.php
+++ about.php
@@ -8,7 +8,7 @@
 
     <!-- Font Awesome Stylesheet -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-  <link rel="stylesheet" href="css/about.css"> 
+  <link rel="stylesheet" href="css/about.css"> <link rel="stylesheet" href="css/about.css"> 
 </head>
 
 <body>
