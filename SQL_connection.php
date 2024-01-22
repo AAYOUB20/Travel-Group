6e7df6c75d875ba6be2ad989536c62c46dfce0fc
AAYOUB20 <aliayoub2019@gmail.com>
2024-01-22 11:29:57 +0100

    code

--- SQL_connection.php
+++ SQL_connection.php
@@ -1,3 +1,3 @@
 <?php
-$conn = new mysqli("localhost", "prova1", "123", "user");
+$conn = new mysqli("localhost", "prova1", "123", "user"); //creazione di connection al database "user" in localhost con il username  "prova1" e il password "123" 
 ?>
