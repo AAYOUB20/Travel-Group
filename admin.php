40518ef34cd6d937cd252876d83c7fbb3a627653
ALI AYOUB <98781492+AAYOUB20@users.noreply.github.com>
2024-02-11 19:22:46 +0100

    ali

--- admin.php
+++ admin.php
@@ -9,6 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Users Messages</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.ome2/css/bootstrap.min.css" rel="stylesheet">
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
     <style>
         .table-container {
             display: none;
@@ -26,7 +27,6 @@
         }
     </style>
     
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
 </head>
    
 
