2c0f9f7a136780ae71a6fcd0fb640562720eb86a
ALI AYOUB <98781492+AAYOUB20@users.noreply.github.com>
2024-02-13 08:26:38 +0100

    ALI

--- PHP/navbar.php
+++ PHP/navbar.php
@@ -7,8 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Your Company</title>
     <!-- Bootstrap  -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> 
-    <link rel="stylesheet" href="css/navbar.css"> 
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> 
+   <!-- <link rel="stylesheet" href="css/navbar.css"> -->
 </head>
 <body>
 <nav class="navbar navbar-expand-lg  fixed-top">
