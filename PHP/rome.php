2ee65cb5876d87962723bd4968443db768cfb69f
ALI AYOUB <98781492+AAYOUB20@users.noreply.github.com>
2024-02-14 02:15:11 +0100

    Ali

--- PHP/rome.php
+++ PHP/rome.php
@@ -24,7 +24,7 @@ mysqli_close($conn);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rome</title>
-    <link rel="stylesheet" href="css/rome.css"> 
+    <link rel="stylesheet" href="../css/rome.css"> 
 </head>
 
 <body>
--- PHP/srilanka.php
+++ PHP/srilanka.php
@@ -26,7 +26,7 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sri Lanka</title>
-    <link rel="stylesheet" href="css/Srilaka.css"> 
+    <link rel="stylesheet" href="../css/Srilaka.css"> 
 
     <style>
         body {
