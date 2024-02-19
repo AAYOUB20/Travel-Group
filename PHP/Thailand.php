34a8a3ac8edc0a3260f0960c841965c365072596
ALI AYOUB <98781492+AAYOUB20@users.noreply.github.com>
2024-02-19 18:01:16 +0100

    ali

--- PHP/Thailand.php
+++ PHP/Thailand.php
@@ -27,131 +27,13 @@ mysqli_close($conn);
     <title>Thailand</title>
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
     <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-    <style>
-        body {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            height: 100vh;
-            margin: 0;
-            font-family: Arial, sans-serif;
-        }
+    <link rel="stylesheet" href="../CSS/Thailand.css"><!-- link al file css Thailand.css -->
 
-        h1 {
-            text-align: center;
-            margin-top: 10px; 
-        }
-
-        .card-container {
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: space-around;
-            align-items: flex-start;
-        }
-
-        .swiper-container {
-            width: 100%;
-            height: 100%;
-        }
-
-        .swiper-slide {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-        }
-
-        .card {
-            width: 200px;
-            border: 3px solid green;
-            padding: 10px;
-            text-align: center;
-            background-color: white;
-            margin: 5px 10px 10px 10px; 
-            border-radius: 20px;
-            box-sizing: border-box;
-            margin-top: -150px; 
-        }
-
-        .card img {
-            width: 100%;
-            height: 150px;
-            object-fit: cover;
-            border-bottom: 1px solid #ddd;
-            margin-bottom: 10px;
-        }
-
-        .back {
-            color: white;
-            background-color: black;
-            position: absolute;
-            top: 10px;
-            left: 10px;
-            font-size: 30px;
-            cursor: pointer;
-            z-index: 0;
-        }
-
-        .back:hover {
-            background-color: green;
-        }
-
-        @media only screen and (max-width: 600px) {
-            .back {
-                font-size: 15px;
-            }
-
-            .card {
-                text-align: center;
-                width: calc(70% - 20px);
-            }
-        }
-
-        @media only screen and (min-width: 601px) and (max-width: 1024px) {
-            .card {
-                font-size: 15px;
-                width: calc(40% - 20px);
-            }
-        }
-
-        .book-button {
-            cursor: pointer;
-            font-style: bold;
-            font-size: 17px;
-            background-color: black;
-            color: white;
-        }
-
-        .swiper-button-next,
-        .swiper-button-prev {
-            position: absolute;
-            top: 50%;
-            width: 30px;
-            height: 30px;
-            margin-top: -15px;
-            background-size: 30px 30px;
-            cursor: pointer;
-            z-index: 10;
-        }
-
-        .swiper-button-next {
-            right: 500px;
-            background-image: url('https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.1/navigation/next.png');
-        }
-
-        .swiper-button-prev {
-            left: 500px;
-            background-image: url('https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.1/navigation/prev.png');
-        }
-
-        .swiper-pagination {
-            display: none;
-        }
-    </style>
 </head>
 
 <body>
 <?php
-include "navbar.php";
+include "navbar.php";// includere il navbar
 ?>
     <h1>Thailand</h1>
 
@@ -159,7 +41,7 @@ include "navbar.php";
         <div class="swiper-wrapper">
             <div class="swiper-slide">
                 <div class="card">
-                    <img src="https://th.bing.com/th/id/R.03ef2e3873dca778936d8ba802ed3a44?rik=7wbNEwajYBVhXA&pid=ImgRaw&r=0"
+                    <img src="../foto/thailand_1.jpeg"
                         alt="Image 1">
                     <p>Date: <?php echo $dates[0]; ?></p>
                     <p>Price: <?php echo $prices[0]; ?></p>
@@ -169,7 +51,7 @@ include "navbar.php";
 
             <div class="swiper-slide">
                 <div class="card">
-                    <img src="https://facts.net/wp-content/uploads/2019/12/mathew-schwartz-gsllxmVO4HQ-unsplash.jpg"
+                    <img src="../foto/thailand_2.jpeg"
                         alt="Image 2">
                     <p>Date: <?php echo $dates[1]; ?></p>
                     <p>Price: <?php echo $prices[1]; ?></p>
@@ -197,12 +79,12 @@ include "navbar.php";
             button.addEventListener('click', function () {
                 <?php
                 if (isset($_SESSION['email'])) {
-                    echo 'window.location.href = "booknow.php";';
+                    echo 'window.location.href = "booknow.php";'; // se l'utente è loggato allora si apre la pagina di prenotazione qundo se digita al booking  button
                 } else {
                     echo 'var confirmRedirect = confirm("You need to log in first. Continue or cancel?");
                             if (confirmRedirect) {
-                                window.location.href = "login.php";
-                            }';
+                                window.location.href = "login.php"; 
+                            }';// altrimenti si apre un messaggio che dice che bisogna loggarsi
                 }
                 ?>
             });
