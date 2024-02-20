3ea319328344e9596bf3c67781d8f9e81e3bda25
brahimhmede <48259518+brahimhmede@users.noreply.github.com>
2024-02-20 13:35:40 +0100

    update

--- PHP/SQL_connection.php
+++ PHP/SQL_connection.php
@@ -1,3 +1,3 @@
 <?php
-$conn = new mysqli("slocalhost", "S5012251", "braxhim123", "S5012251"); //creazione di connection al database "user" in localhost con il username  "prova1" e il password "123" 
+$conn = new mysqli("localhost", "S5012251", "braxhim123", "S5012251"); //creazione di connection al database "user" in localhost con il username  "prova1" e il password "123" 
 ?>
