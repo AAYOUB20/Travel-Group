34b2daac58964ce0ac3cb0dade7ea683fa440309
ALI AYOUB <98781492+AAYOUB20@users.noreply.github.com>
2024-02-20 17:21:15 +0100

    collegare con root

--- PHP/SQL_connection.php
+++ PHP/SQL_connection.php
@@ -1,3 +1,4 @@
 <?php
-$conn = new mysqli("localhost", "S5012251", "braxhim123", "S5012251"); //creazione di connection al database "user" in localhost con il username  "prova1" e il password "123" 
+$conn = new mysqli("localhost", "root", "", "user");
+//$conn = new mysqli("localhost", "S5012251", "braxhim123", "S5012251"); //creazione di connection al database "user" in localhost con il username  "prova1" e il password "123" 
 ?>
