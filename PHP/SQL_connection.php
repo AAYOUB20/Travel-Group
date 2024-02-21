f9346a140a639009cefd0482ef772c31b039e546
ALI AYOUB <98781492+AAYOUB20@users.noreply.github.com>
2024-02-21 22:28:30 +0100

    aòi

--- PHP/SQL_connection.php
+++ PHP/SQL_connection.php
@@ -1,5 +1,5 @@
 <?php
 
-$conn = new mysqli("localhost", "root", "", "user");
-//$conn = new mysqli("localhost", "S5018444", "007", "S5018444");  
+//$conn = new mysqli("localhost", "root", "", "user");
+$conn = new mysqli("localhost", "S5018444", "007", "S5018444");  
 ?>
