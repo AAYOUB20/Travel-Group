891c46723041112bae8bcee29adc2518936b67d3
ALI AYOUB <98781492+AAYOUB20@users.noreply.github.com>
2024-02-21 22:57:34 +0100

    ali

--- booknow.php
+++ booknow.php
@@ -1,3 +1,61 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up</title>
+    <link rel="stylesheet" href="../css/registration1.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Sign Up</h1>
+        <form action="registration.php" method="post">
+            <label for="firstname">First name:</label>
+            <input type="text" id="firstname" name="firstname" required>
+            <br><br>
+
+            <label for="lastname">Last name:</label>
+            <input type="text" id="lastname" name="lastname" required>
+            <br><br>
+
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+            <br><br>
+
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password"   required>
+            
+
+            <br><br>
+
+            <input type="submit" value="Sign Up">
+        </form>
+        <p>Already have an account? <a class="login-link" href="login.php">Log In</a></p>
+    </div>
+    <script>
+      document.addEventListener('DOMContentLoaded', function() {
+          const form = document.querySelector('form');
+          const password = document.getElementById('password');
+          const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+    password.addEventListener('input', function() {
+        if (!passwordPattern.test(password.value)) {
+            password.setCustomValidity('Password should contain at least 8 characters , at least one uppercase letter, one lowercase letter, one number, and one special character.');
+        } else {
+            password.setCustomValidity('');
+        }
+    });
+
+    form.addEventListener('submit', function(event) {
+        if (!password.checkValidity()) {
+            event.preventDefault();
+        }
+    });
+});
+    </script>
+</html>
+
 <?php
 session_start();
 include "navbar.php";
